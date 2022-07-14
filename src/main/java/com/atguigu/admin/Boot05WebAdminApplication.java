d4656364d5b705928e4b48591063d353be7f92f5
LingSha <LingSha@qq.com>
2022-07-14 10:42:44 +0800

    idea first commit

---
+++ src/main/java/com/atguigu/admin/Boot05WebAdminApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan(basePackages = "com.atguigu.admin")//指定原生servlet都放在哪里，默认是当前包和子包
+@SpringBootApplication
+public class Boot05WebAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot05WebAdminApplication.class, args);
+        System.out.println("hello,git!");
+        System.out.println("Sss");
+    }
+
+}
